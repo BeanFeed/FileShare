a12ab5de59a037b22dde3dc010c4bb8b2bed6ddb
BeanFeed <two.austin.two@gmail.com>
2024-01-29 13:22:28 -0500

    Make icon clickable

--- src/components/DirectoryCard.vue
+++ src/components/DirectoryCard.vue
@@ -22,7 +22,7 @@ function nextDirectory(name) {
 <template>
 <div class="md:flex items-center flex-col border-4 border-slate-950 h-auto w-[12rem] rounded-2xl py-3">
   <div class="flex h-36 w-full items-center justify-center">
-    <i class="bi bi-folder text-9xl text-teal-400 hover:bg-white hover:bg-opacity-5 cursor-pointer px-3 py-1 rounded-lg"></i>
+    <i @click="nextDirectory(dirName)" class="bi bi-folder text-9xl text-teal-400 hover:bg-white hover:bg-opacity-5 cursor-pointer px-3 py-1 rounded-lg"></i>
   </div>
   <div class="flex flex-col items-center">
     <a @click="nextDirectory(dirName)" class="cursor-pointer hover:bg-white hover:bg-opacity-5 text-white hover:text-white px-3 py-1 my-1 rounded-lg">{{dirName}}</a>
