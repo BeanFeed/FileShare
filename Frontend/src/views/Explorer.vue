d72ad065c2de7bb1d8e9fca9bd0c898c129dd38f
BeanFeed <two.austin.two@gmail.com>
2024-01-30 00:49:23 -0500

    Add Axios

--- Frontend/src/views/Explorer.vue
+++ Frontend/src/views/Explorer.vue
@@ -1,19 +1,22 @@
 <script setup>
 
 import DirectoryCard from "../components/DirectoryCard.vue";
+import {onMounted} from "vue";
+import axios from "axios";
+/*
+onMounted(() => {
+  let directoriesReq = axios({
+    method: "GET",
+    url: 
+  });  
+});
+*/
 </script>
 
 <template>
   <div class="flex justify-center px-5 pt-6">
     <div class="mx-auto grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 2.5xl:grid-cols-7 gap-4">
-      <DirectoryCard dir-name="Test1" item-count="10"/>
-      <DirectoryCard dir-name="Test2" item-count="10"/>
-      <DirectoryCard dir-name="Test3" item-count="10"/>
-      <DirectoryCard dir-name="Test4" item-count="10"/>
-      <DirectoryCard dir-name="Test5" item-count="10"/>
-      <DirectoryCard dir-name="Test6" item-count="10"/>
-      <DirectoryCard dir-name="Test7" item-count="10"/>
-
+      <DirectoryCard dir-name="Test Folder" item-count="0"/>
 
     </div>
   </div>
