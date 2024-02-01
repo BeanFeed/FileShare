9bc981fc0ef6a728137cdc805c9d4c0789604fb1
BeanFeed <two.austin.two@gmail.com>
2024-02-01 00:13:30 -0500

    Make DAL

---
+++ Backend/DAL/Context/MyDbContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using DAL.Entities;
+
+namespace DAL.Context
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("User");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Passhash).HasColumnName("passhash");
+
+                entity.Property(e => e.Username).HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Backend/DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public partial class User
+    {
+        public byte[] Username { get; set; } = null!;
+        public long Id { get; set; }
+        public byte[] Passhash { get; set; } = null!;
+    }
+}
