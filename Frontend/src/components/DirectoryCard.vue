3ba361469a292f645b8c4738371e7e3ea49e5452
BeanFeed <two.austin.two@gmail.com>
2024-02-01 16:29:17 -0500

    Restyle

--- Frontend/src/components/DirectoryCard.vue
+++ Frontend/src/components/DirectoryCard.vue
@@ -7,6 +7,7 @@ const props = defineProps(["dirName","itemCount"])
 const dPath = ref();
 watchEffect(() => {
   dPath.value = route.params.Directory;
+  console.log(dPath.value)
 })
 function nextDirectory(name) {
   
@@ -19,7 +20,7 @@ function nextDirectory(name) {
 </script>
 
 <template>
-<div class="md:flex items-center flex-col border-4 border-slate-950 h-auto w-[12rem] rounded-2xl py-3">
+<div class="md:flex items-center flex-col border-4 border-slate-950 h-[17rem] w-[12rem] rounded-2xl py-3">
   <div class="flex h-36 w-full items-center justify-center">
     <i @click="nextDirectory(dirName)" class="bi bi-folder text-9xl text-teal-400 hover:bg-white hover:bg-opacity-5 cursor-pointer px-3 py-1 rounded-lg"></i>
   </div>
